8cb75b736dd0f80d62884640529833f76924fc5a
Swann Pincet <swann.pincet@gmail.com>
2021-02-23 11:45:37 +0100

    init + ignore warning logbox

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']);
+
 export default function App() {
   return (
     <View style={styles.container}>
